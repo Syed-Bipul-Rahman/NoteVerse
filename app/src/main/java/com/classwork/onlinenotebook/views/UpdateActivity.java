da478b1bd0b9f685b48b008695235977d24d9da2
Syed-Bipul-Rahman <syedbipulrahman2@gmail.com>
2023-09-11 23:23:52 +0600

    [ADD] : update activity added for update data

---
+++ app/src/main/java/com/classwork/onlinenotebook/views/UpdateActivity.java
@@ -0,0 +1,16 @@
+package com.classwork.onlinenotebook.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.classwork.onlinenotebook.R;
+
+public class UpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update);
+    }
+}
