0001cc385ed0e096bd2c0fffb61ecf4c3cb013f0
Syed-Bipul-Rahman <syedbipulrahman2@gmail.com>
2023-09-12 23:14:11 +0600

    [UPDATE]: binding add to update activity

--- app/src/main/java/com/classwork/onlinenotebook/views/UpdateActivity.java
+++ app/src/main/java/com/classwork/onlinenotebook/views/UpdateActivity.java
@@ -3,14 +3,20 @@ package com.classwork.onlinenotebook.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.classwork.onlinenotebook.R;
+import com.classwork.onlinenotebook.databinding.ActivityUpdateBinding;
 
 public class UpdateActivity extends AppCompatActivity {
-
+ActivityUpdateBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_update);
+
+        binding=ActivityUpdateBinding.inflate(getLayoutInflater());
+        View view=binding.getRoot();
+
+        setContentView(view);
     }
 }
